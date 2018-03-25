8dcdab5833f8c3d7f751ddf3c62ecc3fdde72547
DevAlone <admin@d3d.info>
2018-03-26 01:51:19 +0300

    learning page appearence

--- frontend/src/app/api.service.ts
+++ frontend/src/app/api.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router'
 // import {Observable} from 'rxjs/Observable';
-import {HttpClient} from '@angular/common/http';
-import {catchError} from 'rxjs/operators';
-import {of} from 'rxjs/observable/of';
+import { HttpClient } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+import { of } from 'rxjs/observable/of';
 import { Observable } from "rxjs/Rx"
-import {MessageService} from "./message.service";
+import { MessageService } from "./message.service";
 
 @Injectable()
 export class ApiService {
@@ -22,7 +22,7 @@ export class ApiService {
   }
 
   public login(username: string, password: string): Observable<void> {
-    return this.post('login', {username: username, password: password}).map(res => {
+    return this.post('login', { username: username, password: password }).map(res => {
       if (res.hasOwnProperty('token')) {
         localStorage.setItem('access_token', res.token);
         this.authorized = true;
@@ -36,7 +36,7 @@ export class ApiService {
   }
 
   public register(username: string, password: string, token: string): Observable<void> {
-    return this.post('register', {username: username, password: password, token: token}).map(res => {
+    return this.post('register', { username: username, password: password, token: token }).map(res => {
       if (res.hasOwnProperty('token')) {
         localStorage.setItem('access_token', res.token);
         this.authorized = true;
@@ -68,16 +68,16 @@ export class ApiService {
         observer.next(result);
         observer.complete();
       }, error => {
-          if (error.status === 401) {
-            localStorage.removeItem('access_token');
-            this.authorized = false;
-            this.router.navigateByUrl('/login');
-          }
+        if (error.status === 401) {
+          localStorage.removeItem('access_token');
+          this.authorized = false;
+          this.router.navigateByUrl('/login');
+        }
 
-          this.messageService.error(error.error.error_message);
+        this.messageService.error(error.error.error_message);
 
-          observer.error(error);
-        });
+        observer.error(error);
+      });
     });
 
     // if (!url.startsWith('/')) {
